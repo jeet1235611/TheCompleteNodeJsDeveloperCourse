b06b876e7dcf0717af3901ddbc513a28373e47f9
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-30 17:09:47 +0530

    Create appBetter.js

---
+++ appBetter.js
@@ -0,0 +1,27 @@
+console.log('Starting app.js');
+const fs = require('fs');
+
+fs.appendFileSync('greetings.txt', 'Hellow world!');
+
+// For the higher version of node.
+//Original line
+fs.appendFile('greetings.txt', 'Hello World!');
+
+//option1 
+fs.appendFile('greetings.txt', 'Hello world!', function(err){
+  if(err) {
+    console.log('Unable to write to file');
+  }
+});
+
+//Otion two
+fs.appendFileSync('greetings.txt', 'Hello World!');
+
+//
+// const fs = require('fs');
+// const os = require('os');
+// const notes = require('./notes.js');
+//
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', 'Hello ${user.username}! You are {notes.age}.');
